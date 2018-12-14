bbc1dc5c9384db4a7bb96daa27a2a55b14b8e2cb
Miha <mihadebeljak4@gmail.com>
2018-12-14 20:02:00 +0100

    Initial Commit

---
+++ UdderMilk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UdderMilk
+//
+//  Created by Miha Debeljak on 14/12/2018.
+//  Copyright © 2018 d4400b. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
